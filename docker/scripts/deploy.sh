933a6c2e7ed42679c25f0bb3429ba564a6f6f24c
Deepak Kumar <deepak.kumar2@bekaert.com>
2024-01-20 16:33:18 +0530

    Docker Update

--- docker/scripts/deploy.sh
+++ docker/scripts/deploy.sh
@@ -12,30 +12,45 @@ then
 	echo "*********************** setup static content deploy - setup:static-content:deploy -f  *******************" 
 	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:static-content:deploy -f
 	echo "*********************** Permission ***********************************************"
-  	          chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
+  chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
 fi
 
+if [ "$1" == "-full_no_index" ]
+then
+
+	echo "*********************** Upgrading setup - setup:upgrade ****************************"
+	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:upgrade
+  echo "*********************** Setup compile - setup:di:compile *******************************"
+  php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:di:compile
+	echo "*********************** setup static content deploy - setup:static-content:deploy -f  *******************"
+	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:static-content:deploy -f
+	echo "*********************** Permission ***********************************************"
+  chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
+fi
 
 if [ "$1" == "-compile" ]
 then
 
 	echo "*********************** Setup compile - setup:di:compile *******************************" 
 	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:di:compile
-
+	echo "*********************** Permission ***********************************************"
+  chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
 fi
 if [ "$1" == "-index" ]
 then
 
 	echo "*********************** setup reindex - indexer:reindex ********************************"
 	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento indexer:reindex
-
+	echo "*********************** Permission ***********************************************"
+  chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
 fi
 if [ "$1" == "-content" ]
 then
 
 	echo "*********************** setup static content deploy - setup:static-content:deploy -f  *******************" 
 	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:static-content:deploy -f
-
+	echo "*********************** Permission ***********************************************"
+  chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
 fi
 if [ "$1" == "-upgrade_content" ]
 then
@@ -44,7 +59,8 @@ then
 	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:upgrade
 	echo "*********************** setup static content deploy - setup:static-content:deploy -f  *******************" 
 	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:static-content:deploy -f
-
+	echo "*********************** Permission ***********************************************"
+  chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
 fi
 if [ "$1" == "-permission" ]
 then
@@ -58,6 +74,7 @@ fi
 if [ "$1" == "-help" ]
 then
 	echo -e "Full Deploy   =>>>   { deploy.sh -full }"
+	echo -e "Full Deploy No Index   =>>>   { deploy.sh -full_no_index }"
 	echo -e "Setup upgrade =>>>   { deploy.sh -upgrade_content }"
   echo -e "Setup Index =>>>     { deploy.sh -index }"
 	echo -e "Setup Deploy =>>>    { deploy.sh -compile }"
